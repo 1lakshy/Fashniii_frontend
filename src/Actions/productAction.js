5a5156d2d9896e98df3c2833c6045647ccbb983e
1lakshy <lakshy18sharma@gmail.com>
2024-07-16 23:25:50 +0530

    backend added

--- src/Actions/productAction.js
+++ src/Actions/productAction.js
@@ -10,10 +10,15 @@ import {
     PRODUCT_DETAILS_FAIL,
   } from '../Constants/productConstants';
 
+  const api = axios.create({
+    baseURL: 'https://fashniii-backend.onrender.com',
+  });
+
+  const BASE_URL = "https://fashniii-backend.onrender.com";
   export const getProducts = (keyword="",currentPage=1,price=[0,10000]) => async(dispatch)=>{
     try{
         dispatch({type: ALL_PRODUCT_REQUEST});
-        let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`
+        let link = `${BASE_URL}/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`
         const {data} = await axios.get(link);
 
         dispatch({
@@ -34,7 +39,7 @@ import {
     try{
         dispatch({type: PRODUCT_DETAILS_REQUEST});
 
-        const {data} = await axios.get(`/api/v1/product/${id}`);
+        const {data} = await axios.get(`${BASE_URL}/api/v1/product/${id}`);
 
         dispatch({
             type:PRODUCT_DETAILS_SUCCESS,
